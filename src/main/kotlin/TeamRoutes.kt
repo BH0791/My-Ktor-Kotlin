780618c8a5f2304e99c2ca76b7e872f1efb0b4db
HamidB <ejecteur@yahoo.fr>
2024-12-15 15:52:19 +0100

    Define API

--- src/main/kotlin/TeamRoutes.kt
+++ src/main/kotlin/TeamRoutes.kt
@@ -4,8 +4,24 @@ import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Routing.teamRoutes(){
-    get("/teams"){
+fun Routing.teamRoutes() {
+    get("/teams") {
         call.respond(HttpStatusCode.OK)
     }
+
+    get("/teams/{team_id}") {}
+
+    post("/teams") {}
+
+    put("/teams/{team_id}"){}
+
+    delete("/teams/{team_id}"){}
+
+    get("/teams/{team_id}/players"){}
+
+    post("/teams/{team_id}/players"){}
+
+    put("/teams/{team_id}/players/{player_id}"){}
+
+    delete("/teams/{team_id}/players/{player_id}"){}
 }
