b5d6a03988a67735f357267474858c2fa14b5f8c
HamidB <ejecteur@yahoo.fr>
2024-12-18 04:53:59 +0100

    Update file

---
+++ src/README.md
@@ -0,0 +1,12 @@
+# Développement d'API avec ktor
+
+## Sommaire
+1. Introduction
+   1. Qu'est-ce que ktor
+   2. Comparaison avec le framework Spring
+   3. Le temps de démarrage
+   4. L'écosystème
+   5. La performance
+   6. Créer une application serveur en utilisant ktor
+   7. Choisir un moteur HTTP 
+2. Les base de ktor
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,17 +2,22 @@ package fr.hamtec
 import fr.hamtec.plugins.configureContentNegotiation
 import fr.hamtec.plugins.configureRequestValidation
 import fr.hamtec.plugins.configureStatusPages
+import fr.hamtec.routes.teamRoutes
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import org.slf4j.LoggerFactory
+import java.io.File
 
 fun main(args: Array<String>): Unit =  io.ktor.server.cio.EngineMain.main(args)
 
 fun Application.module() {
+    val logger = LoggerFactory.getLogger("Application")
     routing {
         configureContentNegotiation()
         configureRequestValidation()
         configureStatusPages()
-
+        staticFiles("/static", File("static"))
         teamRoutes()
     }
 
